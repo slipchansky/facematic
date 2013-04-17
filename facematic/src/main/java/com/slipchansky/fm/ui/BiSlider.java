0d4191c563aaef643cc25d8af91e14b2db544282
Stanislav Lipchansky <slipchansky@bigmir.net>
2013-04-17 08:03:58 +0300

    new file:   facematic/src/main/java/com/slipchansky/fm/ui/BiSlider.java 	new file:   va/src/main/java/com/stas/va/Bislider.java 	modified:   va/src/main/resources/com/slipchansky/markup/testform.xml

---
+++ facematic/src/main/java/com/slipchansky/fm/ui/BiSlider.java
@@ -0,0 +1,7 @@
+package com.slipchansky.fm.ui;
+
+import com.vaadin.ui.CustomComponent;
+
+public class BiSlider extends CustomComponent {
+
+}
---
+++ va/src/main/java/com/stas/va/Bislider.java
@@ -0,0 +1,7 @@
+package com.stas.va;
+
+import com.vaadin.ui.CustomComponent;
+
+public class Bislider extends CustomComponent {
+
+}
