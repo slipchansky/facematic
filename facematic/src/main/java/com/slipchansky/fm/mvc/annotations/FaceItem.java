a4b6a47d3302d6aff6b2387310d233c5fc108c33
Stanislav Lipchanky <stas@custvox.com>
2013-04-19 16:54:02 +0300

    staged

--- facematic/src/main/java/com/slipchansky/fm/mvc/annotations/FaceItem.java
+++
@@ -1,8 +0,0 @@
-package com.slipchansky.fm.mvc.annotations;
-
-import java.lang.annotation.Retention;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface FaceItem {
-	String path () default "";
-}
